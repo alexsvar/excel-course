83f2bac88334e0ef539e2ad8fb3dbea97c9c257e
Alexander Morozov <alexsvar@icloud.com>
2020-07-15 16:05:23 +0300

    finish redux logic and debugging

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -1,37 +1,72 @@
+import {toInlineStyles} from '@core/utils'
+import {defaultStyles} from '@/constants'
+import {parse} from '@core/parse'
+
 const CODES = {
   A: 65,
   Z: 90
 }
 
-function toCell(row) {
+const DEFAULT_WIDTH = 120
+const DEFAULT_HEIGHT = 24
+
+function getWidth(state, index) {
+  return (state[index] || DEFAULT_WIDTH) + 'px'
+}
+
+function getHeight(state, index) {
+  return (state[index] || DEFAULT_HEIGHT) + 'px'
+}
+
+function toCell(state, row) {
   return function(_, col) {
+    const id = `${row}:${col}`
+    const width = getWidth(state.colState, col)
+    const data = state.dataState[id]
+    const styles = toInlineStyles({
+      ...defaultStyles,
+      ...state.stylesState[id]
+    })
     return `
       <div
         class="excel__cell"
         contenteditable
         data-col="${col}"
         data-type="cell"
-        data-id="${row}:${col}"
-      ></div>
+        data-id="${id}" 
+        data-value="${data || ''}"
+        style="${styles}; width: ${width}"
+      >${parse(data) || ''}</div>
     `
   }
 }
 
-function toCol(col, index) {
+function toCol({col, index, width}) {
   return `
-    <div class="excel__column" data-type="resizable" data-col="${index}">
+    <div
+        class="excel__column"
+        data-type="resizable"
+        data-col="${index}"
+        style="width: ${width}"
+    >
         ${col}
         <div class="excel__column--resize" data-resize="col"></div>
     </div>      
   `
 }
 
-function createRow(index, content) {
+function createRow(index, content, state) {
   const resize = index
     ? '<div class="excel__row--resize" data-resize="row"></div>'
     : ''
+  const height = getHeight(state, index)
   return `
-    <div class="excel__row" data-type="resizable">
+    <div 
+        class="excel__row"
+        data-type="resizable"
+        data-row="${index}"
+        style="height: ${height}"
+    >
         <div class="excel__row-info">
             ${index ? index : ''}
             ${resize}
@@ -45,25 +80,33 @@ function toChar(_, index) {
   return String.fromCharCode(CODES.A + index)
 }
 
-export function createTable(rowsCount = 15) {
+function withWidthFrom(state) {
+  return (col, index) => {
+    return {
+      col, index, width: getWidth(state.colState, index)
+    }
+  }
+}
+
+export function createTable(rowsCount = 15, state = {}) {
   const colsCount = CODES.Z - CODES.A + 1
   const rows = []
   const cols = new Array(colsCount)
       .fill('')
       .map(toChar)
+      .map(withWidthFrom(state))
       .map(toCol)
       .join('')
 
-  rows.push(createRow(null, cols))
+  rows.push(createRow(null, cols, {}))
 
   for (let row = 0; row < rowsCount; row++) {
     const cells = new Array(colsCount)
         .fill('')
-        // .map(toCell)
-        .map(toCell(row))
+        .map(toCell(state, row))
         .join('')
 
-    rows.push(createRow(row + 1, cells))
+    rows.push(createRow(row + 1, cells, state.rowState))
   }
   return rows.join('')
 }
---
+++ src/components/toolbar/toolbar.template.js
@@ -0,0 +1,74 @@
+function toButton(button) {
+  const meta = `
+    data-type='button'
+    data-value='${JSON.stringify(button.value)}'
+  `
+  return `
+    <div 
+        class="excel__button ${button.active ? 'active' : ''}"
+        ${meta}
+    >
+        <i 
+            class="material-icons"
+            ${meta}
+        >${button.icon}</i>
+    </div>
+  `
+}
+
+export function createToolbar(state) {
+  const buttons = [
+    {
+      value: {textAlign: 'left'},
+      icon: 'format_align_left',
+      active: state['textAlign'] === 'left'
+    },
+    {
+      value: {textAlign: 'center'},
+      icon: 'format_align_center',
+      active: state['textAlign'] === 'center'
+    },
+    {
+      value: {textAlign: 'right'},
+      icon: 'format_align_right',
+      active: state['textAlign'] === 'right'
+    },
+    {
+      value: {
+        fontWeight: state['fontWeight'] === 'bold'
+          ? 'normal'
+          : 'bold'
+      },
+      icon: 'format_bold',
+      active: state['fontWeight'] === 'bold'
+    },
+    {
+      value: {
+        fontStyle: state['fontStyle'] === 'italic'
+          ? 'normal'
+          : 'italic'
+      },
+      icon: 'format_italic',
+      active: state['fontStyle'] === 'italic'
+    },
+    {
+      value: {
+        textDecoration: state['textDecoration'] === 'underline'
+          ? 'none'
+          : 'underline'
+      },
+      icon: 'format_underline',
+      active: state['textDecoration'] === 'underline'
+    },
+    {
+      value: {
+        textDecoration: state['textDecoration'] === 'line-through'
+          ? 'none'
+          : 'line-through'
+      },
+      icon: 'format_strikethrough',
+      active: state['textDecoration'] === 'line-through'
+    }
+  ]
+  return buttons.map(toButton).join('')
+}
